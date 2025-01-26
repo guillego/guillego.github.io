1aeccf449bb29deaaab6b531ab9b03ac7744b660
Guillermo Galan <ggo@guillego.com>
2025-01-26 14:09:28 +0100

    configure nix flake for rust environment

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Photo processor development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        
+        # Read rust-toolchain.toml if it exists, otherwise use stable
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+            openssl
+          ];
+
+          shellHook = ''
+            echo "Rust development environment loaded"
+          '';
+        };
+      }
+    );
+}
